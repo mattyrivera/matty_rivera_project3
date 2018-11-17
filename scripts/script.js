a1025f38f94a1256646c6cfaf4a079de28012631
mattrivera <rivera182matty@gmail.com>
2018-11-16 20:58:03 -0500

    scripts updated

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,16 @@
 // -init
-
 const catQuiz = {};
-const answers = [];
+catQuiz.answers = [];
 const cats = [
     {
         cat : "Sphynx",
-        // img: "",
-        // desc: "",
+        img: "assets/img-1.jpg",
+        desc: "soooo cute",
     },
     {
         cat: "Russian Blue",
         // img: "",
-        // desc: "",
+        desc: "literally the cutest cat ever",
 
     },
     {
@@ -26,6 +25,8 @@ const cats = [
     }
 ];
 
+console.log(cats);
+
 catQuiz.init = function(){
     catQuiz.calculateResults();
 };
@@ -34,54 +35,67 @@ catQuiz.calculateResults = function(){
     $('form').on('submit', function(event){
         event.preventDefault();
         // console.log('yayyy');
+        catQuiz.answers = [];
 
         //This is to get the length of questions.
         let questions = $(".fieldset-container").length;
 
         //Loop through all questions and answers.
         //Loop for question (5)
-        for (i = 1; i <= questions; i++){
+        for (let i = 1; i <= questions; i++){
             //Loop for answers (3)
-            for(j = 1; j <= 3; j++){
+            for(let j = 1; j <= 3; j++){
                 //If input is checked get value
                 if($(`input.cat-q${i}-a${j}`).is(":checked")){
                     //Get the answer value and convert into a number
                     let answer = parseInt($(`input.cat-q${i}-a${j}`).val());
                     //Push numbers into answers array
-                    answers.push(answer);
+                    catQuiz.answers.push(answer);
                 }
             }
         }
-        console.log(answers);
+        console.log(catQuiz.answers);
 
         //Add up values from answers array for total score
+        total = 0;
+        for (k = 0; k < catQuiz.answers.length; k++){
+            total += catQuiz.answers[k];
+        }
+        console.log(total);
 
         //"If statements for results section."
-        if (answers == 5 && answers < 8) {
-            $(".result-container").append(`
+        $(".results-container").html("");
+        if (total >= 5 && total <= 7) {
+            $(".results-container").append(`
             <h2 class="result-title">${cats[0].cat}<h2>
-            <img class="result-img src"${cats[0].img}">
+            <img class="result-img" src="${cats[0].img}">
             <p class="result-desc">${cats[0].desc}</p>
             `)
-        } else if (answers == 8 && answers < 11) {
-            $(".result-container").append(`
-            <h2 class="result-title">${cats[0].cat}<h2>
-            <img class="result-img src"${cats[0].img}">
-            <p class="result-desc">${cats[0].desc}</p>
+        } 
+
+        // FIX THE CODE IN THESE ANSWERS
+
+        else if (total >= 8 && total <= 10) {
+            $(".results-container").append(`
+            <h2 class="result-title">${cats[1].cat}<h2>
+            <img class="result-img src"${cats[1].img}">
+            <p class="result-desc">${cats[1].desc}</p>
             `)
-        } else if (answers == 11 && answers < 13) {
-            $(".result-container").append(`
-            <h2 class="result-title">${cats[0].cat}<h2>
-            <img class="result-img src"${cats[0].img}">
-            <p class="result-desc">${cats[0].desc}</p>
+        } 
+        else if (total >= 11 && total <= 12) {
+            $(".results-container").append(`
+            <h2 class="result-title">${cats[2].cat}<h2>
+            <img class="result-img src"${cats[2].img}">
+            <p class="result-desc">${cats[2].desc}</p>
             `)
-        } else if (answers == 13 && answers < 16) {
-            $(".result-container").append(`
-            <h2 class="result-title">${cats[0].cat}<h2>
-            <img class="result-img src"${cats[0].img}">
-            <p class="result-desc">${cats[0].desc}</p>
+        } 
+        else if (total >= 13 && total <= 15) {
+            $(".results-container").append(`
+            <h2 class="result-title">${cats[3].cat}<h2>
+            <img class="result-img src"${cats[3].img}">
+            <p class="result-desc">${cats[3].desc}</p>
             `)
-        }
+        };
     });
 };
 
