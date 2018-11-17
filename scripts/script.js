a1f66a60fea87226302cd2d109b05de70ddb5fc9
mattrivera <rivera182matty@gmail.com>
2018-11-17 13:29:48 -0500

    styles updated

--- scripts/script.js
+++ scripts/script.js
@@ -4,24 +4,24 @@ catQuiz.answers = [];
 const cats = [
     {
         cat : "Sphynx",
-        img: "assets/img-1.jpg",
+        img: "assets/img-2.jpg",
         desc: "soooo cute",
     },
     {
         cat: "Russian Blue",
-        // img: "",
+        img: "assets/img-1.jpg",
         desc: "literally the cutest cat ever",
 
     },
     {
         cat: "Persian",
-        // img: "",
-        // desc: "",
+        img: "assets/img-3.jpg",
+        desc: "fluffy baby",
     },
     {
         cat: "Domestic Shorthair",
-        // img: "",
-        // desc: ""
+        img: "assets/img-4.jpg",
+        desc: "happy cat"
     }
 ];
 
@@ -64,7 +64,8 @@ catQuiz.calculateResults = function(){
         console.log(total);
 
         //"If statements for results section."
-        $(".results-container").html("");
+        // $(".results-container").html("");
+
         if (total >= 5 && total <= 7) {
             $(".results-container").append(`
             <h2 class="result-title">${cats[0].cat}<h2>
@@ -78,21 +79,21 @@ catQuiz.calculateResults = function(){
         else if (total >= 8 && total <= 10) {
             $(".results-container").append(`
             <h2 class="result-title">${cats[1].cat}<h2>
-            <img class="result-img src"${cats[1].img}">
+            <img class="result-img" src="${cats[1].img}">
             <p class="result-desc">${cats[1].desc}</p>
             `)
         } 
         else if (total >= 11 && total <= 12) {
             $(".results-container").append(`
             <h2 class="result-title">${cats[2].cat}<h2>
-            <img class="result-img src"${cats[2].img}">
+            <img class="result-img" src="${cats[2].img}">
             <p class="result-desc">${cats[2].desc}</p>
             `)
         } 
         else if (total >= 13 && total <= 15) {
             $(".results-container").append(`
             <h2 class="result-title">${cats[3].cat}<h2>
-            <img class="result-img src"${cats[3].img}">
+            <img class="result-img" src="${cats[3].img}">
             <p class="result-desc">${cats[3].desc}</p>
             `)
         };
