297c967af4b0a469eeb1b5190be614842ccf970a
mattrivera <rivera182matty@gmail.com>
2018-11-18 11:53:23 -0500

    styles updated

--- scripts/script.js
+++ scripts/script.js
@@ -9,20 +9,20 @@ const cats = [
     },
     {
         cat: "Russian Blue",
-        img: "assets/img-1.jpg",
+        img: "assets/nap1.jpg",
         desc: "literally the cutest cat ever",
 
     },
     {
         cat: "Persian",
-        img: "assets/img-3.jpg",
+        img: "assets/persiantest1.png",
         desc: "fluffy baby",
-    },
-    {
-        cat: "Domestic Shorthair",
-        img: "assets/img-4.jpg",
-        desc: "happy cat"
     }
+    // {
+    //     cat: "Domestic Shorthair",
+    //     img: "assets/img-4.jpg",
+    //     desc: "happy cat"
+    // }
 ];
 
 console.log(cats);
@@ -66,7 +66,7 @@ catQuiz.calculateResults = function(){
         //"If statements for results section."
         // $(".results-container").html("");
 
-        if (total >= 5 && total <= 7) {
+        if (total >= 5 && total <= 8) {
             $(".results-container").append(`
             <h2 class="result-title">${cats[0].cat}<h2>
             <img class="result-img" src="${cats[0].img}">
@@ -76,26 +76,26 @@ catQuiz.calculateResults = function(){
 
         // FIX THE CODE IN THESE ANSWERS
 
-        else if (total >= 8 && total <= 10) {
+        else if (total >= 9 && total <= 12) {
             $(".results-container").append(`
             <h2 class="result-title">${cats[1].cat}<h2>
             <img class="result-img" src="${cats[1].img}">
             <p class="result-desc">${cats[1].desc}</p>
             `)
         } 
-        else if (total >= 11 && total <= 12) {
+        else if (total >= 13 && total <= 15) {
             $(".results-container").append(`
             <h2 class="result-title">${cats[2].cat}<h2>
             <img class="result-img" src="${cats[2].img}">
             <p class="result-desc">${cats[2].desc}</p>
             `)
-        } 
-        else if (total >= 13 && total <= 15) {
-            $(".results-container").append(`
-            <h2 class="result-title">${cats[3].cat}<h2>
-            <img class="result-img" src="${cats[3].img}">
-            <p class="result-desc">${cats[3].desc}</p>
-            `)
+        // } 
+        // else if (total >= 13 && total <= 15) {
+        //     $(".results-container").append(`
+        //     <h2 class="result-title">${cats[3].cat}<h2>
+        //     <img class="result-img" src="${cats[3].img}">
+        //     <p class="result-desc">${cats[3].desc}</p>
+        //     `)
         };
     });
 };
